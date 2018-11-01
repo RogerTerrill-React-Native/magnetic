bc37f2750a37019fb3476d16ddf740f75f9ebc4e
Roger Terrill <rchicasterill@gmail.com>
2018-11-01 10:29:25 -0700

    Component Level State

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('component will mount in albumlist');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response.data));
   }
   render() {
     return (
